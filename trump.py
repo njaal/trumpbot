1f3b521c87d6d8af5056c6848f8b864dc2bf39e0
Kristin Bergan Grimstad <Kristin@Kristins-MacBook-Air.local>
2019-01-11 11:30:34 +0100

    no message

--- trump.py
+++ trump.py
@@ -30,7 +30,7 @@ previousTrumpHeaders = r.get('trump')
 
 #print("previous:" + previousTrumpHeaders)
 if previousTrumpHeaders is not None:
-    previousTrumpHeaders = previousTrumpHeaders.decode('string_escape')
+    previousTrumpHeaders = previousTrumpHeaders.decode('unicode_escape')
 print ("tidligere header: "+previousTrumpHeaders)
 
 for header in headers:
